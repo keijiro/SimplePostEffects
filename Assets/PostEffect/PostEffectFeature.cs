27e0c6afd5d73de46a1de3e15db7d80561613ef7
Keijiro Takahashi <keijiro@gmail.com>
2022-03-02 16:02:02 +0900

    Invert effect sample

---
+++ Assets/PostEffect/PostEffectFeature.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace UrpTestbed {
+
+sealed class PostEffectPass : ScriptableRenderPass
+{
+    public Material material;
+
+    public override void Execute
+      (ScriptableRenderContext context, ref RenderingData data)
+    {
+        if (material == null) return;
+        var cmd = CommandBufferPool.Get("PostEffect");
+        Blit(cmd, ref data, material, 0);
+        context.ExecuteCommandBuffer(cmd);
+        CommandBufferPool.Release(cmd);
+    }
+}
+
+public sealed class PostEffectFeature : ScriptableRendererFeature
+{
+    public Material material;
+
+    PostEffectPass _pass;
+
+    public override void Create()
+      => _pass = new PostEffectPass
+           { material = material,
+             renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing };
+
+    public override void AddRenderPasses
+      (ScriptableRenderer renderer, ref RenderingData data)
+      => renderer.EnqueuePass(_pass);
+}
+
+} // namespace UrpTestbed
